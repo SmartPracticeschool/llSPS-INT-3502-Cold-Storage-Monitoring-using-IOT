e7ce93079fa3eb8d0260069f4d46ad363fc15019
yaswanthmasireddy <68846775+yaswanthmasireddy@users.noreply.github.com>
2020-07-27 19:33:11 +0530

    Add files via upload

---
+++ cold storage monitoring.py
@@ -0,0 +1,52 @@
+import time
+import sys
+import ibmiotf.application
+import ibmiotf.device
+import random
+#Provide your IBM Watson Device Credentials
+organization = "cef01x"
+deviceType = "raspberrypi"
+deviceId = "123456"
+authMethod = "token"
+authToken = "12345678"
+
+# Initialize GPIO
+
+def myCommandCallback(cmd):
+        print("Command received: %s" % cmd.data)
+        i=cmd.data['command']
+        if i=='motoron':
+                print("motor is on")
+        elif i=='motoroff':
+                print("motor is off")
+        
+
+try:
+    deviceOptions = {"org": organization, "type": deviceType, "id": deviceId, "auth-method": authMethod, "auth-token": authToken}
+    deviceCli = ibmiotf.device.Client(deviceOptions)
+    #..............................................
+	
+except Exception as e:
+    print("Caught exception connecting device: %s" % str(e))
+    sys.exit()
+
+# Connect and send a datapoint "hello" with value "world" into the cloud as an event of type "greeting" 10 times
+deviceCli.connect()
+
+while True:
+    hum=random.randint(10,40)
+    #print(hum)
+    temp =random.randint(30,80)
+    light=random.randint(10,50)
+    #Send Temperature & Humidity to IBM Watson
+    data = { 'Temperature' : temp, 'Humidity': hum,'lightintensity' : light }
+    #print (data)
+    def myOnPublishCallback():
+        print ("Published Temperature = %s C" % temp, "Humidity = %s %%" % hum,"lightintensity= %s C" % light,"to IBM Watson")
+    success = deviceCli.publishEvent("DHT11", "json", data, qos=0, on_publish=myOnPublishCallback)
+    if not success:
+        print("Not connected to IoTF")
+    time.sleep(2)
+    deviceCli.commandCallback = myCommandCallback
+# Disconnect the device and application from the cloud
+deviceCli.disconnect()
